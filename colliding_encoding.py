608b6d967d16cafcd173c2ed34de2f60907710b1
zedricchan <125533831+zedricchan@users.noreply.github.com>
2023-04-17 10:03:26 +0800

    Create colliding_encoding.py

---
+++ colliding_encoding.py
@@ -0,0 +1,14 @@
+T = input()
+digit = input()
+digits = digit.split()
+def test():
+    N = input()
+    strings = []
+    for i in range(N):
+        string = input()
+    if string in strings:
+        return 'YES'
+    else:
+        return 'NO'
+for n in range(T):
+    print(test())
